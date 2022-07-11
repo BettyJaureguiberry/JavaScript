69474177e80a4c197b765a8406ef27eb6a86daec
Betty Jaureguiberry <soledadjaureguiberry@gmail.com>
2022-07-10 21:54:34 -0300

    simulador

--- js/main.js
+++ js/main.js
@@ -1,15 +1,47 @@
-let numero1 = 0;
-let numero2= 1;
-const espiral=[];
-const valor1=0;
-const valor2=1;
+const productos = ["Saquitos masa philo", "Pulpo sobre pastel de papa", "Vieiras a la plancha sobre pure de limón y pimiento","Gyozas de rabo de toro a la cordobesa"];
+const precios= [1500,2000,1800,1200];
+let pedido;
+let cantidad;
+let aPagar;
+let nombrecomp;
+let direccion;
+let telefono;
+let datoAdic;
+let orden;
+let datosPersonales;
 
-let cantidad= prompt ("Por favor ingrese la cantidad de valores que quiere conocer de la espiral de Fibonacci:");
+function tomarPedido (){
+    do{
+        pedido=prompt("Bienvenidos! Que desean ordenar: 1-Saquitos masa philo  2-Pulpo sobre pastel de papa  3-Vieiras a la plancha sobre pure de limón y pimiento  4-Gyozas de rabo de toro a la cordobesa");
+    } while (pedido>4); 
 
-for ( let i=0; i<(cantidad-2); i++){
-    let rdo= numero1 + numero2;
-    espiral[i]=rdo; 
-    numero1=numero2;
-    numero2=rdo;
+    do{
+        cantidad=prompt("Cuantas porciones desea ordenar:");
+    } while (cantidad<0);
+
+    aPagar=(cantidad * precios[pedido]);
 }
-document.write (valor1+ ","+ valor2+ "," +espiral);
+
+
+function datosEnvio(){
+    nombrecomp = prompt("Por favor ingrese su nombre completo:");
+    direccion =prompt ("Ingrese por favor su direccion:");
+    telefono= prompt ("Por favor ingrese un telefono de contacto:");
+    datoAdic=prompt("Desea ingresar informacion adicional para la entrega del pedido:");
+}
+
+function ordenPedido(){
+    document.write("Gracias por su compra, a la brevedad llegara su pedido. Bom Apetit!")
+    document.write(orden);
+    document.write(" $",cantidad * precios[pedido]);
+    document.write(datosPersonales);
+}
+
+function validarDatos(){
+    orden= productos[pedido] + " " + cantidad;
+    datosPersonales= nombrecomp + " " + direccion + " " + telefono + " " + datoAdic;
+}
+tomarPedido();
+datosEnvio();
+validarDatos();
+ordenPedido(orden, aPagar, datosPersonales);
