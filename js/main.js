1b4a4dbbb30239835e65006446d0acd13e68d1d5
Betty Jaureguiberry <soledadjaureguiberry@gmail.com>
2022-08-07 21:49:16 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-const pedidoFinales=[];
-let aPagar=pedidoFinales.reduce((acumulador, elemento)=>acumulador+elemento.price,0);
-document.getElementById("carrito-total").innerHTML = `Cantidad Pedida:${pedidoFinales.length} - Monto a Pagar:$${aPagar}`;
+
+const prodCarrito=JSON.parse(localStorage.getItem("carrito"));
+const pedidoFinales=prodCarrito;
+const tcarrito=localStorage.getItem('carrito');
+document.getElementById("carTotalEncabezado").innerHTML= tcarrito;
+aPagar=pedidoFinales.reduce((acumulador, elemento)=>acumulador+elemento.price,0);
+document.getElementById("carTotalEncabezado").innerHTML = pedidoFinales.length + "- $" + aPagar;
+
+//Declaracion de variables
+
 
 
 const productos = [
@@ -40,15 +47,20 @@ const pedido=[];
 const cantidad=[];
 const orden=[];
 
-
-
 let contPedido=0;
 let contCant=0; 
 let opMenu;
 let opCant;
 let index=0;
 let cantFinal;
+const nombre ="";
+const direccion ="";
+const email="";
+const telefono="";
+const mensaje="";
+const mostrarErrores="";
 
+//Creamos las cards
 productos.forEach ((producto) => {
     const idBoton=`add-cart${producto.id}`
     document.getElementById("cards").innerHTML +=
@@ -81,123 +93,172 @@ productos.forEach ((producto) => {
         `
     });
 
-
+carritoStorage();
 productos.forEach((producto)=>{
     const idBoton=`add-cart${producto.id}`
     document.getElementById(idBoton).addEventListener('click', ()=> {
-    const rdo= pedidoFinales.some((el) => el.id == producto.id);
-    console.log(rdo);
-    if (rdo==true){
-        //const cursos = pedidoFinales.map((producto) => { 
-            //if (producto.id === pedidoFinales.id){
-                pedidoFinales.cantidad++;
-                pedidoFinales.monto=pedidoFinales.cantidad * pedidoFinales.price;
-                const eliminar=document.getElementById(`${pedidoFinales.length}`);
-                pedidoFinales.splice(eliminar, 1);
-                document.getElementById("itemsCarrito").innerHTML ="";
-                for (const pedidoFinal of pedidoFinales){
-                    document.getElementById("itemsCarrito").innerHTML += `
-                        <tr>
-                            <th scope="row">${pedidoFinal.id}</th>
-                            <td>${pedidoFinal.title}</td>
-                            <td>${pedidoFinal.cantidad}</td>
-                            <td>${pedidoFinal.price}</td>
-                            <td><button onclick='eliminarDelCarrito()'  id="${pedidoFinales.length}"  class="btn btn-danger" width=10px>X</button></td>
-                        </tr>`;
-    };
-            //}
-            
-        //});
-        
-    }
-    pedidoFinales.push(producto);
-    cantFinal=pedidoFinales.reduce((acumulador, elemento)=>acumulador+elemento.cantidad,0);
-    aPagar=pedidoFinales.reduce((acumulador, elemento)=>acumulador+elemento.monto,0);//GRACIAS!!...ahi vi donde me estaba equivocando! ;)
-    mostrarIngCarrito(producto, pedidoFinales);
-    document.getElementById("carritoTotal").innerHTML = `Cantidad Pedida:${cantFinal} - Monto a Pagar:$${aPagar}`;
-
-    })
+        pedidoFinales.push(producto);
+        localStorage.setItem("tcarrito", JSON.stringify(tcarrito));
+        mostrarIngCarrito(producto);
+        calcularCarrito(producto.price);
+    });
 });
 
-function eliminarDelCarrito (){ 
-    const eliminar=document.getElementById(`${pedidoFinales.length}`);
-    pedidoFinales.splice(eliminar, 1);
-    aPagar=pedidoFinales.reduce((acumulador, elemento)=>acumulador+elemento.monto,0);
-    document.getElementById("carritoTotal").innerHTML = `Cantidad Pedida:${pedidoFinales.length} - Monto a Pagar:$${aPagar}`;
+function carritoStorage(){
     document.getElementById("itemsCarrito").innerHTML ="";
-    for (const pedidoFinal of pedidoFinales){
+    calcularCarrito();
+    pedidoFinales.forEach((pedidoFinal)=>{
         document.getElementById("itemsCarrito").innerHTML += `
                 <tr>
                     <th scope="row">${pedidoFinal.id}</th>
                     <td>${pedidoFinal.title}</td>
                     <td>${pedidoFinal.cantidad}</td>
                     <td>${pedidoFinal.price}</td>
-                    <td><button onclick='eliminarDelCarrito()'  id="${pedidoFinales.length}"  class="btn btn-danger" width=10px>X</button></td>
+                    <td><button onclick='eliminarDelCarrito("${pedidoFinal.id}")' id="${pedidoFinal.id}"  class="btn btn-danger" width=10px>X</button></td>
                 </tr>`;
-    };
-};     
+                document.getElementById("carritoTotal").innerHTML = `Cantidad Pedida:${pedidoFinales.length} - Monto a Pagar:$${aPagar}`;
+                document.getElementById("carTotalEncabezado").innerHTML= pedidoFinales.length + "- $" + aPagar;
 
+});
+    carritoPop();    
+};
 
-//
-function mostrarIngCarrito(producto, pedidoFinales){  
+//CALCULA CANTIDAD Y MONTO A PAGAR
+function calcularCarrito(){
+    aPagar=pedidoFinales.reduce((acumulador, elemento)=>acumulador+elemento.price,0);
+    localStorage.setItem("tcarrito",JSON.stringify(pedidoFinales));
+};
+
+//MUESTRA LO CARGADO EN EL CARRITO
+function mostrarIngCarrito(pedidoFinales){  
+    calcularCarrito();
     
     document.getElementById("itemsCarrito").innerHTML += `
                 <tr>
-                    <th scope="row">${producto.id}</th>
-                    <td>${producto.title}</td>
-                    <td>${producto.cantidad}</td>
-                    <td>${producto.price}</td>
-                    <td><button onclick='eliminarDelCarrito()' id="${pedidoFinales.length}"  class="btn btn-danger" width=10px>X</button></td>
+                    <th scope="row">${pedidoFinales.id}</th>
+                    <td>${pedidoFinales.title}</td>
+                    <td>${pedidoFinales.cantidad}</td>
+                    <td>${pedidoFinales.price}</td>
+                    <td><button onclick='eliminarDelCarrito("${pedidoFinales.id}")' id="${pedidoFinales.id}"  class="btn btn-danger" width=10px>X</button></td>
                 </tr>`;
-};
-/*const botonEliminar = pedidoFinales.indexOf();
-    console.log("indexof"+botonEliminar);
-//
-/*function mostrarIngCarrito (tituloProd){
-    alert ("Se ingreso al pedido:"+tituloProd);
-};*/
+    document.getElementById("carritoTotal").innerHTML = `Cantidad Pedida:${pedidoFinales.length} - Monto a Pagar:$${aPagar}`;
+    document.getElementById("carTotalEncabezado").innerHTML= pedidoFinales.length  + "- $" + aPagar;
+    carritoPop(pedidoFinales);
 
 
-document.getElementById("botonEnviar").addEventListener('click', () => {
-    const nombre=document.getElementById("nombre");
-    const direccion=document.getElementById("apellido");
-    const email=document.getElementById("email");
-    const telefono=document.getElementById("telefono");
-    const mostrarErrores=document.getElementById("errores");
     
-    let error="";
-    let emailOk=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let validacion=false;
+};
 
-    if(nombre.value.length < 6){
-        error += 'El nombre no es valido';
-    }
-    if (!emailOk.test(email.value)){
-        error += 'El mail no es valido';
-    }
-    if (direccion.value.length <5){
-        error += 'El apellido no es valido';
-    }
-    if (isNaN(telefono)){
-        error += 'El telefono no es valido';
-    }
-    if(validacion){
-        mostrarErrores.innerHTML = error;
-    }else {
-        mostrarErrores.innerHTML = "Pedido Enviado";
-    }
+function carritoPop(){
+    console.log ("aca estoy");
+    pedidoFinales.forEach((pedidoFinal) => {  
+    document.getElementById("itemsPOP").innerHTML += `
+                <tr>
+                    <th scope="row">${pedidoFinal.id}</th>
+                    <td>${pedidoFinal.title}</td>
+                    <td>${pedidoFinal.cantidad}</td>
+                    <td>${pedidoFinal.price}</td>
+                    <td><button onclick='eliminarDelCarrito("${pedidoFinal.id}")' id="${pedidoFinal.id}"  class="btn btn-danger" width=10px>X</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">${nombre}</th>
+                    <td>${direccion}</td>
+                    <td>${email}</td>
+                    <td>${telefono}</td>
+                    <td>${mensaje}</td>
+                `;
+    document.getElementById("carritoTotal").innerHTML = `Cantidad Pedida:${pedidoFinales.length} - Monto a Pagar:$${aPagar}`;
+    document.getElementById("carTotalEncabezado").innerHTML= pedidoFinales.length + "- $" + aPagar;
+});
+};
 
+document.getElementById("botonCarrito").addEventListener('click', ()=> {
+    console.log ("botonCarrito");
+    carritoPop();
 });
 
+//MUESTRA EL CARRITO DESPUES DE ELIMIAR UN ITEMS
+function reescribirIngCarrito(pedidoFinales){ 
+    document.getElementById("itemsCarrito").innerHTML ="";
+    calcularCarrito();
+    pedidoFinales.forEach((pedidoFinal)=>{
+        document.getElementById("itemsCarrito").innerHTML += `
+                <tr>
+                    <th scope="row">${pedidoFinal.id}</th>
+                    <td>${pedidoFinal.title}</td>
+                    <td>${pedidoFinal.cantidad}</td>
+                    <td>${pedidoFinal.price}</td>
+                    <td><button onclick='eliminarDelCarrito("${pedidoFinal.id}")' id="${pedidoFinal.id}"  class="btn btn-danger" width=10px>X</button></td>
+                </tr>`;
+                document.getElementById("carritoTotal").innerHTML = `Cantidad Pedida:${pedidoFinales.length} - Monto a Pagar:$${aPagar}`;
+                document.getElementById("carTotalEncabezado").innerHTML= pedidoFinales.length  + "- $" + aPagar;
+                
 
-class datosEnvio{
+});
+    carritoPop();    
+};
+
+//ELIMINAR DEL CARRITO
+function eliminarDelCarrito(productoid){
+    const ped=pedidoFinales.find((productos) => (productos.id)== productoid);
+    console.log(ped);
+    
+    const index = pedidoFinales.indexOf(ped);
+    console.log(index);
+    if (index !=-1){
+        pedidoFinales.splice(index, 1);
+    };
+    reescribirIngCarrito(pedidoFinales);
+    calcularCarrito();
+    
+}
+
+//FORMULARIO DATOS ENVIO
+nombre=document.getElementById("nombre");
+direccion=document.getElementById("apellido");
+email=document.getElementById("email");
+telefono=document.getElementById("telefono");
+mensaje=document.getElementById("mensaje");
+mostrarErrores=document.getElementById("errores");
+let mal="";
+    nombre.addEventListener('change', () => {
+        if(nombre.value.length < 5){
+            mal = 'El nombre no es valido';
+            mostrarErrores.innerHTML = mal;
+            }
+        });
+    direccion.addEventListener('change', () => {
+        if (direccion.value.length<5){
+            mal = 'La direccion no es valido';
+            mostrarErrores.innerHTML = mal;
+            }
+        });
+    email.addEventListener('change', () => {
+        let emailOk=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!emailOk.test(email.value)){
+            mal = 'El mail no es valido';
+            mostrarErrores.innerHTML = mal;
+            }
+        });
+    telefono.addEventListener('change', () => {
+        if (isNaN(telefono)){
+            mal = 'El telefono no es valido';
+            mostrarErrores.innerHTML = mal;
+            }
+        });
+
+document.getElementById("botonEnviar").addEventListener('click', () => {
+    carritoPop();
+    alert("Pedido Enviado");
+});
+    
+
+
+/*class datosEnvio{
     constructor(nombrecomp, direccion,telefono, datoAdic){
         this.nombrecomp=nombrecomp;
         this.direccion=direccion;
         this.telefono=telefono;
         this.datoAdic=datoAdic;
     };
-};
-
-
-
+};*/
